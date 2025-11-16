01eff98a6423f51b847ee914a00c18c9923428d8
YashGupta-24 <yashgupta.241103@gmail.com>
2025-11-16 16:41:14 +0530

    service initialization; localDb initialized

--- src/main/java/Service/UserBookingService.java
+++ src/main/java/Service/UserBookingService.java
@@ -1,5 +1,17 @@
 package Service;
 
+import Entities.User;
+
+import java.io.File;
+
 public class UserBookingService {
+    private User user;
+
+    private static final String USERPATH="../localDb/users.json";
+
+    UserBookingService(User user){
+        this.user=user;
+        File users=new File(USERPATH);
+    }
 
 }
